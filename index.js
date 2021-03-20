ead0e5c3aa2c789dea7a3600d8d5970fc61e4c1e
KubaQPL <61653162+KubaQPL@users.noreply.github.com>
2021-03-20 20:24:30 +0100

    Second

--- index.js
+++ index.js
@@ -4,20 +4,43 @@ var Apka = /** @class */ (function () {
     }
     Apka.prototype.start = function () {
         this.getInputs();
-        this.chVal();
+        this.checkValues();
     };
     Apka.prototype.getInputs = function () {
-        this.Input1 = document.querySelector("#input1");
-        this.Input2 = document.querySelector("#input2");
-        this.Input3 = document.querySelector("#input3");
-        this.Input4 = document.querySelector("#input4");
-        this.sum_input = document.querySelector("sum");
-        this.sum_input = document.querySelector("min");
-        this.sum_input = document.querySelector("max");
-        this.sum_input = document.querySelector("avg");
+        this.input1 = document.querySelector('#input1');
+        this.input2 = document.querySelector('#input2');
+        this.input3 = document.querySelector('#input3');
+        this.input4 = document.querySelector('#input4');
+        this.sum_input = document.querySelector('#sum');
+        this.min_input = document.querySelector('#min');
+        this.max_input = document.querySelector('#max');
+        this.avg_input = document.querySelector('#avg');
     };
-    Apka.prototype.chVal = function () {
+    Apka.prototype.checkValues = function () {
+        var _this = this;
+        this.input1.addEventListener('input', function () { return _this.getCalculatedValues(); });
+        this.input2.addEventListener('input', function () { return _this.getCalculatedValues(); });
+        this.input3.addEventListener('input', function () { return _this.getCalculatedValues(); });
+        this.input4.addEventListener('input', function () { return _this.getCalculatedValues(); });
+    };
+    Apka.prototype.getCalculatedValues = function () {
+        var value1 = +this.input1.value;
+        var value2 = +this.input2.value;
+        var value3 = +this.input3.value;
+        var value4 = +this.input4.value;
+        var sum = value1 + value2 + value3 + value4;
+        var min = Math.min(value1, value2, value3, value4);
+        var max = Math.max(value1, value2, value3, value4);
+        var avg = sum / 4;
+        this.showResults(sum, avg, min, max);
+    };
+    Apka.prototype.showResults = function (sum, avg, min, max) {
+        this.sum_input.value = sum.toString();
+        this.min_input.value = min.toString();
+        this.max_input.value = max.toString();
+        this.avg_input.value = avg.toString();
     };
     return Apka;
 }());
+;
 var app = new Apka();
